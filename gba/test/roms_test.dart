5e32dc832de168d1a315cae0cacfad1cbd62f43f
Matan Lurey <matan@lurey.org>
2017-02-19 00:51:47 -0800

    Try to get ROMs tested.

--- gba/test/roms_test.dart
+++ gba/test/roms_test.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:arm7_tdmi/arm7_tdmi.dart';
+import 'package:gba/src/cartridge/header.dart';
 import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
@@ -14,9 +16,23 @@ void main() {
 
 void _run(File file) {
   test('should pass ${p.basename(file.path)}', () {
-    final cpu = new Arm7Tdmi(gprs: new Arm7TdmiRegisters());
-    final instructions = file.readAsBytesSync();
-    final program = const Arm7TdmiCompiler().compile(cpu, instructions);
-    program.step();
-  }, skip: 'Not yet supported');
+    final rom = file.readAsBytesSync();
+    final header = new CartridgeHeaderReader(
+      new Uint32List.fromList(rom).buffer,
+    );
+    final offset = header.entryPoint != 0 ? header.entryPoint : 0x14F;
+    final formats = rom
+      .skip(offset)
+      .map((i) {
+        try {
+          return new Arm7TdmiInstructionFormat.decoded(i);
+        } catch (e) {
+          return e.toString();
+        }
+      })
+      .toList();
+    for (var i = 0; i < formats.length; i++) {
+      print('[${(offset + i).toRadixString(2)}]: ${formats[i]}');
+    }
+  });
 }
